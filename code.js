d8e9a6c09098654e319398358e9d5f09f573593e
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-26 00:10:20 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    // distances holds the distances from any given node to the start node
+    const distances = {};
+    // unexplored holds all nodes yet to be traversed
+    const unexplored = {};
+
+    // using test code retrieved from fellow student Collin Davis's repository
+
+    // for each node in the graph, initialize distances
+    for(const node in graph) {
+        distances[node] = 0;
+        unexplored[node] = true;
+    }
+
+    distances[sourceNode] = 0;
+    let nextNode = sourceNode;
+
+    while(unexplored.includes(true)) {
+        let closestNode = null;
+        for(let connectedNode in graph[nextNode]) {
+            if(closestNode === null || graph[nextNode][connectedNode] < nextNode[closestNode]){
+                closestNode = connectedNode;
+            }
+            if(distances[nextNode] + graph[nextNode][connectedNode] < distances[connectedNode]) {
+                distances[connectedNode] = distances[nextNode] + graph[nextNode][connectedNode];
+            }
+        }
+        unexplored[closestNode] = false;
+    }
+
+    return distances;
 }
