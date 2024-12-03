18c0dace961f8dbfe6a30589c820ec0b03f60628
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-12-03 15:16:28 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,19 @@
 function dijkstra(graph, sourceNode) {
+    // declare an empty array size = num of nodes
     let distances = new Array (graph.length);
+    // declare a set with one object per node
     let unexplored = new Set(Object.keys(graph));
 
+    // fill distance array with infinity
     for (const node in graph) {
         distances[node] = Infinity;
     }
-    
+
+    // set the distance from sourceNode to sourceNode = 0
     distances[sourceNode] = 0;
+    // while there are unexplored nodes
     while (unexplored.size > 0) {
+        // reset this variable
         let closestNode = null;
 
         // Find the closest unexplored node
@@ -17,11 +23,13 @@ function dijkstra(graph, sourceNode) {
             }
         });
 
+        // if no closest unexplored, then the graph is disconnected
         if (closestNode === null || distances[closestNode] === Infinity) {break};
 
+        // remove closestNode from unexplored, as it is now explored
         unexplored.delete(closestNode);
 
-        // Update distances for neighbors
+        // Update distances for neighbors using the values from the current node
         for (const neighbor in graph[closestNode]) {
             const newDistance = distances[closestNode] + graph[closestNode][neighbor];
             if (newDistance < distances[neighbor]|| distances[neighbor] === Infinity) {
